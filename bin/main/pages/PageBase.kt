98478a7639e9e0cf871e662307247acbef75caf4
Reinaldo Rossetti <reinaldo.rossetti@paygo.com.br>
2021-10-07 15:05:54 -0300

    adicionando o pom.xml

--- bin/main/pages/PageBase.kt
+++
@@ -1,9 +0,0 @@
-package pages
-
-import com.microsoft.playwright.Page
-import com.microsoft.playwright.options.WaitForSelectorState
-
-open class PageBase() {
-    val stateATTACHED: Page.WaitForSelectorOptions = Page.WaitForSelectorOptions().setState(WaitForSelectorState.ATTACHED)
-    val stateDETACHED: Page.WaitForSelectorOptions = Page.WaitForSelectorOptions().setState(WaitForSelectorState.DETACHED)
-}
